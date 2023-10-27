13bf8f5d9364b57959265d0cc398f57da933794d
“TatyanaA” <t.alchikova@gmail.com>
2023-10-27 16:17:18 +0100

    Add counter

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const colors =require('ansi-colors');
 const readline = require('readline');
 const options = ["Rock", "Paper", "Scissors"]; //options for computer
-
+let counter=0
 //For prompt 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -9,7 +9,7 @@ const rl = readline.createInterface({
 });
 
 let recursiveQuestion=function(){
-    rl.question('Whoud you like to play Rock,Paper,Scissors?(type exit for exit): ',(ans0)=>{
+    rl.question(`Whoud you like to play Rock,Paper,Scissors. Your current score is ${counter}?(type exit for exit):` ,(ans0)=>{
         if(ans0==="exit") {
             rl.close();
         }  
@@ -19,26 +19,41 @@ let recursiveQuestion=function(){
                 const randomIndex = Math.floor(Math.random() * options.length);
                 const randomChoice = options[randomIndex];
 
-                if(ans1==="Rock" && randomChoice==="Paper") {
+                //Rock
+                if(ans1.toLowerCase()==="rock" && randomChoice==="Paper") {
                     console.log(colors.red(`You lost! Try again! Computer choice was: ${randomChoice}`)) 
                 }
-                else if (ans1==="Rock" && randomChoice==="Rock") {
-                    console.log(colors.yellow("It is a draw!")) 
-                }
-                else if (ans1==="Rock" && randomChoice==="Scissors") {
+
+                else if (ans1.toLowerCase()==="rock" && randomChoice==="Scissors") {
                     console.log(colors.green(`You are the winner!Computer choice was:${randomChoice }`)) 
+                    counter+=1;
                 }
 
-                else if (ans1==="Paper" && randomChoice==="Paper") {
-                    console.log(colors.yellow("It is a draw!")) 
+                //Paper
+                else if (ans1.toLowerCase()==="paper" && randomChoice==="Scissors") {
+                    console.log(colors.red(`You lost! Try again! Computer choice was: ${randomChoice}`))  
                 }
-                else if (ans1==="Paper" && randomChoice==="Scissors") {
+                else if (ans1.toLowerCase()==="paper" && randomChoice==="Rock") {
+                    console.log(colors.green(`You are the winner!Computer choice was:${randomChoice }`)) 
+                    counter+=1;
+                }
+
+
+                //Scissors
+                else if (ans1.toLowerCase()==="scissors" && randomChoice==="Paper") {
+                    console.log(colors.green(`You are the winner!Computer choice was:${randomChoice }`)) 
+                    counter+=1;
+                }
+                else if (ans1.toLowerCase()==="scissors" && randomChoice==="Rock") {
                     console.log(colors.red(`You lost! Try again! Computer choice was: ${randomChoice}`))  
                 }
 
-                else if (ans1==="Scissors" && randomChoice==="Scissors") {
+                //Draw
+                else if (ans1.toLowerCase()===randomChoice.toLowerCase()) {
                     console.log(colors.yellow("It is a draw!")) 
                 }
+
+                else {console.log(colors.blue(`Something goes wrong, please,check you typed rock, paper, scissors`))} 
 
                 recursiveQuestion();
             
